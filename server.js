546b5e0b8271af826075fa825164508699e80ed9
Najm Sheikh <najm97@gmail.com>
2015-08-08 17:21:45 -0400

    mkay

--- server.js
+++ server.js
@@ -12,10 +12,29 @@ app.use(bodyParser.urlencoded({
 
 
 
-app.post('/message', function(req, res){
-    console.log(req.body);
-    res.end();
+app.post('/message', function(req, res) {
+    if (isRequest) {
+        console.log('Tis a novice!');
+    }
+    if (isSubmission) {
+        console.log('Tis a pro!');
+    }
 });
+
+function isRequest(msg) {
+    if ((msg.indexOf('I want to try out') > -1)) || (msg.indexOf('I would like to try') > -1) || (msg.indexOf('I feel like trying') > -1) {
+        return true;
+    }
+    return false;
+};
+
+function isSubmission() {
+    if ((msg.indexOf('I am known for') > -1)) || (msg.indexOf('I can help find') > -1) || (msg.indexOf('I can help try') > -1) {
+        return true;
+    }
+    return false;
+}
+
 
 app.listen(process.env.PORT, function() {
     console.log("Server is up  running at port: " + process.env.PORT);
